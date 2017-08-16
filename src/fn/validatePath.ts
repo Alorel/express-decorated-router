5e2b67cf335d39c6758d8e1084875cf0e9cb9837
Alorel <a.molcanovas@gmail.com>
2017-08-16 18:53:51 +0100

    parse functions

---
+++ src/fn/validatePath.ts
@@ -0,0 +1,18 @@
+import {endsWith, isString, startsWith} from 'lodash';
+
+/**
+ * Validate the path, throwing an error if it's invalid
+ * @param {string} path The path to validate
+ * @throws {Error} If the validation fails
+ */
+export const validatePath = (path: string): void => {
+  if (!path) {
+    throw new Error('Path is required');
+  } else if (!isString(path)) {
+    throw new Error('Path must be a string');
+  } else if (!startsWith(path, '/')) {
+    throw new Error('Path must start with a slash');
+  } else if (path.length > 1 && endsWith(path, '/')) {
+    throw new Error('Path must not end with a slash');
+  }
+};
