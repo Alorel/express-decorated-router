2133170b3b150c20974f80795654bc01694bf47c
Arturas Molcanovas <a.molcanovas@gmail.com>
2018-01-25 23:18:40 +0000

    rm example dist

--- example/dist/MockDB.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MockDB = new Map();
-exports.MockDB.set(1, { id: 1, size: 'tiny' });
-exports.MockDB.set(2, { id: 2, size: 'massive' });
