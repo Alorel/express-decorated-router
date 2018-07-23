3e4dfb4a5b8b79c226517709b7263b17505c7c8c
Arturas <a.molcanovas@gmail.com>
2018-07-23 20:12:46 +0100

    test: rm RequestHandler types from Middleware.ts

--- example/src/Middleware.ts
+++ example/src/Middleware.ts
@@ -1,11 +1,11 @@
 import * as bodyParser from 'body-parser';
-import {Request, RequestHandler, Response} from 'express';
+import {Request, Response} from 'express';
 import {LazyGetter} from 'typescript-lazy-get-decorator';
 
 export class Middleware {
 
   @LazyGetter()
-  public static get blogs(): RequestHandler {
+  public static get blogs() {
     return (_req: Request, res: Response, next: any) => {
       res.header('is-blog', '1');
       next();
@@ -13,7 +13,7 @@ export class Middleware {
   }
 
   @LazyGetter()
-  public static get comments(): RequestHandler {
+  public static get comments() {
     return (_req: Request, res: Response, next: any) => {
       res.header('is-comment', '1');
       next();
@@ -21,7 +21,7 @@ export class Middleware {
   }
 
   @LazyGetter()
-  public static get forAllUsers(): RequestHandler {
+  public static get forAllUsers() {
     return (_req: Request, res: Response, next: any) => {
       res.header('is-user', '1');
       next();
@@ -29,7 +29,7 @@ export class Middleware {
   }
 
   @LazyGetter()
-  public static get jsonBodyParser(): RequestHandler {
+  public static get jsonBodyParser() {
     return bodyParser.json();
   }
 }
